ec57a4a1c9ea1d5d69d327a314a940977a9cd449
Katerina <katerinalupacheva@gmail.com>
2020-02-28 15:09:27 +0300

    On input change and on onput submit functions added

--- src/components/user-input/user-input.component.js
+++ src/components/user-input/user-input.component.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './user-input.styles.scss';
 
-const UserInput = () => (
-    <form className='user-input-form'>
+const UserInput = () => {
+    const[user, setUser] = useState('');
+    const onInputChange = e => setUser(e.target.value);
+    
+    const onInputSubmit = e => {
+        e.preventDefault();
+        console.log('USER ' + user)
+    }
+    return(
+    <form className='user-input-form' onSubmit={onInputSubmit}>
         <label className='user-input-label' htmlFor='username'>Enter GitHub's Username</label>
-        <input className='user-input' type='text' name='username' />
+        <input className='user-input' type='text' name='username' onChange={onInputChange}/>
     </form>
-);
+)};
 
 export default UserInput;
