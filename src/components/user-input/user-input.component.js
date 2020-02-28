c4e0dae449806e0a061f39969d5b8c12ab759a62
Katerina <katerinalupacheva@gmail.com>
2020-02-28 15:45:15 +0300

    Submit button and fetch request added

--- src/components/user-input/user-input.component.js
+++ src/components/user-input/user-input.component.js
@@ -8,11 +8,16 @@ const UserInput = () => {
     const onInputSubmit = e => {
         e.preventDefault();
         console.log('USER ' + user)
+        fetch(`https://api.github.com/users/${user}`)
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response, null, 2)))
     }
+
     return(
     <form className='user-input-form' onSubmit={onInputSubmit}>
         <label className='user-input-label' htmlFor='username'>Enter GitHub's Username</label>
         <input className='user-input' type='text' name='username' onChange={onInputChange}/>
+        <button className='user-input-button' onClick={onInputSubmit}>Submit</button>
     </form>
 )};
 
