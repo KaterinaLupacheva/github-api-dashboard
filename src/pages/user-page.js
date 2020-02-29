7049c77ca2185c77f687fdd7eadca0264f011828
Katerina <katerinalupacheva@gmail.com>
2020-02-29 14:13:18 +0300

    Error for fetch added to user page

--- src/pages/user-page.js
+++ src/pages/user-page.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const UserPage = props => {
+  const [error, setError] = useState({ active: false, status: 200 });
+  const [userInfo, setUserInfo] = useState(null);
   const location = useLocation();
 
   useEffect(() => {
     const user = location.state.user;
     fetch(`https://api.github.com/users/${user}`)
-      .then(response => response.json())
-      .then(response => console.log(JSON.stringify(response, null, 2)));
+      .then(resp => {
+        if (resp.status === 403) {
+          return setError({ active: true, status: 403 });
+        }
+        if (resp.status === 404) {
+          return setError({ active: true, status: 404 });
+        }
+        return resp.json();
+      })
+      .then(json => setUserInfo(json))
+      .catch(error => setError({ active: true, status: 400 }));
   });
 
-  return <div>USER PAGE</div>;
+  return (
+    <div>
+      {error && error.active ? (
+        <div>{error.status}</div>
+      ) : (
+        <>{userInfo && <img src={userInfo.avatar_url} alt="user-avatar" />}</>
+      )}
+    </div>
+  );
 };
 
 export default UserPage;
