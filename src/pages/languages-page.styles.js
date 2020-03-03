6d5bf0063504762c80f56e6ca17b01ffac21d294
Katerina <katerinalupacheva@gmail.com>
2020-03-03 18:21:29 +0300

    Languages page styled

---
+++ src/pages/languages-page.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { colors, fontSizes } from '../global.styles';
+
+export const TotalLanguages = styled.div`
+  padding: 10vh 0 5vh 0;
+  color: ${colors.textColor};
+  font-size: ${fontSizes.medium};
+`;
+
+export const DataRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding-right: 10vw;
+`;
