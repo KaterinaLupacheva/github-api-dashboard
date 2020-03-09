5ce9249685a56f955c4ceab1a091357185b91c8e
Katerina <katerinalupacheva@gmail.com>
2020-03-10 00:01:55 +0300

    Rate limit date format changed

--- src/components/rate-limit/rate-limit.component.js
+++ src/components/rate-limit/rate-limit.component.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const RateLimit = ({ rateLimit }) => {
-  const time = new Date(rateLimit.reset * 1000).toLocaleTimeString('en-US');
+  const time = new Date(rateLimit.reset * 1000).toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return (
     <div style={{ color: 'white' }}>
       {`${rateLimit.remaining} / ${rateLimit.limit} requests remain until ${time}`}
